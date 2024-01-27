bae3d3cd85c1af769ce1499901816c14451de448
roc102 <kyroc102@gmail.com>
2024-01-27 20:14:57 +0530

    final

--- src/components/Footer/Footer.Component.jsx
+++ src/components/Footer/Footer.Component.jsx
@@ -26,12 +26,12 @@ const Footer = () => {
     <div className="bg-gray-800 text-white py-8 lg:pl-16 px-6">
       <div className="container mx-auto flex flex-wrap justify-center lg:justify-between">
         <div className="w-full lg:w-1/4 mb-8 lg:mb-0 text-center lg:text-left">
-          <h3 className="text-xl lg:text-2xl font-semibold mb-4">Contact Us</h3>
+          <h3 className="text-xl lg:text-xl font-semibold mb-4">Contact Us</h3>
           <p>Email: info@example.com</p>
           <p>Phone: +123 456 7890</p>
         </div>
         <div className="w-full lg:w-1/4 mb-8 lg:mb-0 text-center lg:text-left">
-          <h3 className="text-xl lg:text-2xl font-semibold mb-4">Follow Us</h3>
+          <h3 className="text-xl lg:text-xl font-semibold mb-4">Follow Us</h3>
           <div className="flex justify-center lg:justify-start items-center">
             <Link to="#" style={iconStyle}>
               <FaFacebook />
@@ -57,7 +57,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="w-full lg:w-1/4 text-center lg:text-left">
-          <h3 className="text-xl lg:text-2xl font-semibold mb-4">
+          <h3 className="text-xl lg:text-xl font-semibold mb-4">
             Terms and Conditions
           </h3>
           <div className="mb-2">
